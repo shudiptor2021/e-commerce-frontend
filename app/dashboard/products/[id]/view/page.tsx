11419ed85a1b5a58e9f8c76979c7df3ac262f325
Shudipt0 <royshudipto318@gmail.com>
2025-11-08 01:41:36 +0600

    complete product page

--- app/dashboard/products/[id]/view/page.tsx
+++ app/dashboard/products/[id]/view/page.tsx
@@ -1,4 +1,5 @@
 import { getSingleProduct } from "@/lib/auth";
+import Image from "next/image";
 import Link from "next/link";
 
 interface Props {
@@ -8,9 +9,25 @@ interface Props {
 const viewPage = async (props: Props) => {
   const { id } = await props.params;
   const product = await getSingleProduct(id);
-  //   console.log(product.tags);
+  // console.log(product?.thumbnail?.url);
+  const productData = [
+    { name: "id", item: product._id },
+    { name: "title", item: product.title },
+    { name: "price", item: product.price },
+    { name: "category", item: product.category },
+    { name: "stock", item: product.stock },
+    { name: "rating", item: product.rating },
+    { name: "brand", item: product.brand },
+    { name: "warranty Information", item: product.warrantyInformation },
+    { name: "shipping Information", item: product.shippingInformation },
+    { name: "available Status", item: product.availableStatus },
+    { name: "product Age", item: product.productAge },
+    { name: "return Policy", item: product.returnPolicy },
+    { name: "minimumOrder Quantity", item: product.minimumOrderQuantity },
+    { name: "created At", item: product.createdAt },
+  ];
   return (
-    <div>
+    <div className="p-2 md:p-3 lg:p-4">
       {/* top */}
       <div className="w-full flex justify-between items-center ">
         <h1 className="text-muted-foreground">View Product Details</h1>
@@ -20,71 +37,84 @@ const viewPage = async (props: Props) => {
       </div>
 
       {/* product details */}
-      <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-3">
-        <div className="col-span-1 w-fit px-4 py-2 border border-muted-foreground rounded flex items-center gap-4">
-          <h1>id:</h1>
-          <p>{product._id}</p>
+
+      <div className="w-full flex flex-col md:flex-row gap-3 mt-8">
+        {/* left side */}
+        <div className="w-1/2 flex flex-wrap gap-2 lg:gap-5">
+          {productData.map((data, index) => (
+            <div
+              key={index}
+              className=" w-fit px-4 py-2 capitalize border border-muted-foreground rounded flex items-center gap-4"
+            >
+              <h1>{data.name}:</h1>
+              <p>{data.item}</p>
+            </div>
+          ))}
+          <div className=" w-fit px-4 py-2 border border-muted-foreground rounded flex items-center gap-4">
+            <h1>tags:</h1>
+            {product?.tags ? (
+              <ul className="flex items-center gap-2">
+                {product.tags.map((tag: string, index: number) => (
+                  <li key={index}>{tag}</li>
+                ))}
+              </ul>
+            ) : (
+              <p>null</p>
+            )}
+          </div>
         </div>
-        <div className="col-span-1 w-fit px-4 py-2 border border-muted-foreground rounded flex items-center gap-4">
-          <h1>title:</h1>
-          <p>{product.title}</p>
+        {/* right side */}
+        <div className="w-1/2 flex flex-col gap-2 lg:gap-4">
+          <div className=" w-fit px-4 py-2 border border-muted-foreground rounded flex items-center gap-4">
+            <h1>description:</h1>
+            <p>{product.description}</p>
+          </div>
+          {/* image */}
+          <div className=" w-full px-4 py-2 border border-muted-foreground rounded flex gap-4">
+            <h1>Thumnail:</h1>
+            {product?.thumbnail?.url ? (
+              <Image
+                src={product.thumbnail.url}
+                alt={product.thumbnail.public_id}
+                width={100}
+                height={100}
+              />
+            ) : (
+              <p>no thumbnail</p>
+            )}
+          </div>
+
+          <div className=" w-full px-4 py-2 border border-muted-foreground rounded flex gap-2">
+            <h1>Images:</h1>
+            {product?.images && product.images.length > 0 ? (
+              <div>
+                {product.images.map(
+                  (
+                    image: { url: string; public_id: string },
+                    index: number
+                  ) => (
+                    <div key={index}>
+                      <Image
+                        src={image.url}
+                        alt={`Product image || ${image.public_id}`}
+                        width={100}
+                        height={100}
+                      />
+                    </div>
+                  )
+                )}
+              </div>
+            ) : (
+              <p>no images</p>
+            )}
+          </div>
         </div>
-        <div className="col-span-1 w-fit px-4 py-2 border border-muted-foreground rounded flex items-center gap-4">
-          <h1>price:</h1>
-          <p>{product.price}</p>
-        </div>
-        <div className="col-span-1 w-fit px-4 py-2 border border-muted-foreground rounded flex items-center gap-4">
-          <h1>category:</h1>
-          <p>{product.category}</p>
-        </div>
-        <div className="col-span-4 w-fit px-4 py-2 border border-muted-foreground rounded flex items-center gap-4">
-          <h1>description:</h1>
-          <p>{product.description}</p>
-        </div>
-        <div className="col-span-1 w-fit px-4 py-2 border border-muted-foreground rounded flex items-center gap-4">
-          <h1>stock:</h1>
-          <p>{product.stock}</p>
-        </div>
-        <div className="col-span-1 w-fit px-4 py-2 border border-muted-foreground rounded flex items-center gap-4">
-          <h1>rating:</h1>
-          <p>{product.rating}</p>
-        </div>
-        <div className="col-span-1 w-fit px-4 py-2 border border-muted-foreground rounded flex items-center gap-4">
-          <h1>tags:</h1>
-          {product?.tags ? (
-            <ul className="flex items-center gap-2">
-              {product.tags.map((tag: string, index: number) => (
-                <li key={index}>{tag}</li>
-              ))}
-            </ul>
-          ) : (
-            <p>null</p>
-          )}
-        </div>
-        <div className="col-span-1 w-fit px-4 py-2 border border-muted-foreground rounded flex items-center gap-4">
-          <h1>brand:</h1>
-          <p>{product.brand}</p>
-        </div>
-        <div className="col-span-1 w-fit px-4 py-2 border border-muted-foreground rounded flex items-center gap-4">
-          <h1>weight:</h1>
-          <p>{product.weight || "null"}</p>
-        </div>
-        <div className="col-span-1 w-fit px-4 py-2 border border-muted-foreground rounded flex items-center gap-4">
-          <h1>brand:</h1>
-          <p>{product.brand}</p>
-        </div>
-        <div className="col-span-1 w-fit px-4 py-2 border border-muted-foreground rounded flex items-center gap-4">
-          <h1>warrantyInformation:</h1>
-          <p>{product.warrantyInformation}</p>
-        </div>
-        <div className="col-span-1 w-fit px-4 py-2 border border-muted-foreground rounded flex items-center gap-4">
-          <h1>shippingInformation:</h1>
-          <p>{product.shippingInformation}</p>
-        </div>
-        <div className="col-span-1 w-fit px-4 py-2 border border-muted-foreground rounded flex items-center gap-4">
-          <h1>availableStatus:</h1>
-          <p>{product.availableStatus}</p>
-        </div>
+      </div>
+      {/* update button */}
+      <div>
+        <Link href={`/dashboard/products/${product._id}/update`}>
+          <button>update</button>
+        </Link>
       </div>
     </div>
   );
